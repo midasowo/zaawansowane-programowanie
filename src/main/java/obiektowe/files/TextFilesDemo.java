74155c3bb5ae59c5db4ba8185536e71f24465522
midsowo <kmiduch@gmail.com>
2022-10-13 17:44:12 +0200

    simple file writer

---
+++ src/main/java/obiektowe/files/TextFilesDemo.java
@@ -0,0 +1,22 @@
+package obiektowe.files;
+
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class TextFilesDemo {
+
+    public static void main(String[] args) {
+        try {
+            PrintWriter out = new PrintWriter("cat.txt");  //przykładowe narzędzie do zapisu w pliku
+            String car = "passat 1.9 tdi 180 tys. km";
+            out.println(car); //wydruk pliku
+            out.println(car);
+            out.close(); //zamkniecie narzędzia
+        } catch (FileNotFoundException e) { //obsłużyć wyjątki
+            System.out.println("Problem z utworzeniem pliku!");
+            e.printStackTrace();
+        }
+
+    }
+}
